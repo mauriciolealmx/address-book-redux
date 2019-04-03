f31abe16c86f72f0a0b4756a753a5ad2bcac964d
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-03 15:39:59 -0600

    Added signin and signup buttons to header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import styles from './header.styles';
 
 class Header extends Component {
+  handleClick(path) {
+    this.props.history.push(path);
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -18,6 +24,12 @@ class Header extends Component {
             <Typography variant="h6" color="inherit" className={classes.grow}>
               Address Book
             </Typography>
+            <Button onClick={() => this.handleClick('/signin')} color="inherit">
+              Login
+            </Button>
+            <Button onClick={() => this.handleClick('/signup')} color="inherit">
+              Sign up
+            </Button>
           </Toolbar>
         </AppBar>
       </div>
@@ -29,4 +41,4 @@ Header.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Header);
+export default withStyles(styles)(withRouter(Header));
