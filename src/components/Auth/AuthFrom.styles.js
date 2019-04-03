93f4568b1dd5c04810f10990ce38ad4ce8893502
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-03 16:50:12 -0600

    Moved styles to AuthForm.styles

---
+++ src/components/Auth/AuthFrom.styles.js
@@ -0,0 +1,14 @@
+export default () => ({
+  root: {
+    width: 450,
+    height: 480,
+    boxSizing: 'border-box',
+    padding: '48px 40px 36px 40px'
+  },
+
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '5px 0 5px 0'
+  }
+});
