1fb112f00756f76027618ed49b57e1d40611cdda
Mauricio Leal <mauriciolealmx@gmail.com>
2019-04-03 15:39:31 -0600

    Updated AuthForm to have type password.

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
 
+import Paper from '@material-ui/core/Paper';
+
 class SigninForm extends Component {
   state = { email: '', password: '' };
 
   onSubmit = event => {
     event.preventDefault();
     const { email, password } = this.state;
-    this.props.onSubmit({ email, password});
+    this.props.onSubmit({ email, password });
   };
 
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
-        <fieldset>
-          <label>Email</label>
-          <input
-            name="email"
-            value={this.state.email}
-            onChange={event => this.setState({ email: event.target.value })}
-          />
-        </fieldset>
-        <fieldset>
-          <label>Password</label>
-          <input
-            value={this.state.password}
-            onChange={event => this.setState({ password: event.target.value })}
-          />
-        </fieldset>
-        <div>{this.props.errorMessage}</div>
-        <button>Sign in!</button>
-      </form>
+      <Paper>
+        <form onSubmit={this.onSubmit}>
+          <fieldset>
+            <label>Email</label>
+            <input
+              name="email"
+              value={this.state.email}
+              onChange={event => this.setState({ email: event.target.value })}
+            />
+          </fieldset>
+          <fieldset>
+            <label>Password</label>
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={event =>
+                this.setState({ password: event.target.value })
+              }
+            />
+          </fieldset>
+          <div>{this.props.errorMessage}</div>
+          <button>Submit</button>
+        </form>
+      </Paper>
     );
   }
 }
